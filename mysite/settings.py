a89548b9f23f778248954c8df7f4dc47aab84d5e
TuanTrain <TuanTrain@users.noreply.github.com>
2021-07-14 16:23:48 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
